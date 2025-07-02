3248d8beb4a15e838409e3d8bd2627f2d2f8ac73
Huzaifa Mukhtar <mukhtarhuzaifa436@gmail.com>
2025-07-02 06:05:10 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={gilroy.variable}>
+    <html lang="en" className={gilroy.variable} suppressHydrationWarning>
       <body suppressHydrationWarning className={`antialiased`}>
         <SmoothScroll>{children}</SmoothScroll>
       </body>
