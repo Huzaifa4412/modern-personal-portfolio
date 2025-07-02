4e864f273ec3685d2c7ebafb34b1645d119206e6
Huzaifa Mukhtar <mukhtarhuzaifa436@gmail.com>
2025-07-02 06:00:26 +0500

    update

--- src/app/font.ts
+++ src/app/font.ts
@@ -3,28 +3,28 @@ import localFont from 'next/font/local'
 export const gilroy = localFont({
     src: [
         {
-            path: './font/gilroy/Gilroy-Medium.ttf',
+            path: '/fonts/gilroy/Gilroy-Medium.ttf',
             weight: '400',
-            style: 'normal'
+            style: 'normal',
         },
         {
-            path: './font/gilroy/Gilroy-SemiBold.ttf',
+            path: '/fonts/gilroy/Gilroy-SemiBold.ttf',
             weight: '600',
-            style: 'normal'
+            style: 'normal',
         },
     ],
     variable: '--font-gilroy',
-    display: 'swap'
+    display: 'swap',
 })
 
 export const mazius = localFont({
     src: [
         {
-            path: './font/mazius/Mazius-Extraitalic.otf',
+            path: '/fonts/mazius/Mazius-Extraitalic.otf',
             weight: '400',
-            style: 'normal'
+            style: 'normal',
         },
     ],
     variable: '--font-mazius',
-    display: 'swap'
+    display: 'swap',
 })
